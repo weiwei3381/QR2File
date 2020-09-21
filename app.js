5c0823c4b6286e26c1b67c3b3839f1a42463a690
wowbat <weiwei3381@live.com>
2020-09-22 00:03:39 +0800

    feat: 不存在输出文件夹时进行创建

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const fs = require('fs')
 const helper = require('./helper')
 
-const args = process.argv.slice(2)
+// 如果不存在文件夹, 则进行创建
+fs.existsSync('./output') || fs.mkdirSync('./output')
+fs.existsSync('./QR_imgs') || fs.mkdirSync('./QR_imgs')
 
+// 获取参数
+const args = process.argv.slice(2)
+// 帮助参数信息
 if(args[0] === "help" || args[0]==='h'){
   console.log(`
   "s" or "scan" + filename : convert file to QR images in QR_imgs diretory;
-  "f" or "file" + base64 code : conver base64 code to file in output diretory. 
+  "f" or "file" + base64 code or txtfile : conver base64 code to file in output diretory. 
   `)
 }else if((args[0] === "s" || args[0] === "scan") && args[1]){
   const base64String = helper.fileToBase64(args[1])
